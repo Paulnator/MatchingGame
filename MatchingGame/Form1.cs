c547193c266b85051403cf0770ae1110904b4b56
DrPawel <PaulnatorYT@gmail.com>
2024-03-09 12:14:58 +0100

    Added win condition - Playable version

--- MatchingGame/Form1.cs
+++ MatchingGame/Form1.cs
@@ -12,6 +12,9 @@ namespace MatchingGame
 {
     public partial class Form1 : Form
     {
+        Label firstClicked = null;
+        Label secondClicked = null;
+
         public Form1()
         {
             InitializeComponent();
@@ -43,15 +46,62 @@ namespace MatchingGame
 
         private void label1_Click(object sender, EventArgs e)
         {
+            if (timer1.Enabled == true)
+                return;
+
             Label clickedLabel = sender as Label;
 
-            if(clickedLabel != null)
-            {
-                if (clickedLabel.ForeColor == Color.Black)
-                    return;
+            if (clickedLabel.ForeColor == Color.Black)
+                return;
 
-                clickedLabel.ForeColor = Color.Black;
+            if (firstClicked == null)
+            {
+                firstClicked = clickedLabel;
+                firstClicked.ForeColor = Color.Black;
+                return;
             }
+
+            secondClicked = clickedLabel;
+            secondClicked.ForeColor = Color.Black;
+
+            CheckForWinner();
+
+            if (firstClicked.Text == secondClicked.Text)
+            {
+                firstClicked = null;
+                secondClicked = null;
+                return;
+            }
+
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Stop();
+
+            firstClicked.ForeColor = firstClicked.BackColor;
+            secondClicked.ForeColor = secondClicked.BackColor;
+
+            firstClicked = null;
+            secondClicked = null;
+        }
+
+        void CheckForWinner()
+        {
+            foreach (Control control in tableLayoutPanel1.Controls)
+            {
+                Label iconLable = control as Label;
+
+                if (iconLable != null)
+                {
+                    if (iconLable.ForeColor == iconLable.BackColor)
+                        return;
+                }
+            }
+
+            MessageBox.Show("You win, Congratulations");
+            Close();
         }
     }
 }
